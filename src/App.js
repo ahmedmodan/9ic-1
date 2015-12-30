52df62d72ee40428c1c2bd43fe64d8dd9cf20cc7
Ahmed Modan <ahmed.modan@gmail.com>
2015-12-30 14:56:23 -0800

    refactor App entry point for use with Redux

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
-import React, { Component } from 'react';
-import Router from 'react-router';
-import routes from './config/routes';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import createBrowserHistory from 'history/lib/createBrowserHistory';
+import { syncReduxAndRouter } from 'redux-simple-router';
+import routes from './routes';
+import Root from './containers/Root';
+import configureStore from './redux/configureStore';
 
-export default () =>
-    <Router>{routes}</Router>;
+const history = createBrowserHistory();
+const store = configureStore(window.__INITIAL_STATE__);
+
+syncReduxAndRouter(history, store, (state) => state.router);
+
+// Render the React application to the DOM
+ReactDOM.render(
+  <Root history={ history } routes= { routes } store= { store } />,
+  document.getElementById('root')
+  );
