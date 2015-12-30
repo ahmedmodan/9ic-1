ff92e0069795a2552b2e100a65124b7c66794f52
Ahmed Modan <ahmed.modan@gmail.com>
2015-12-30 14:01:32 -0800

    create initial reducer to get genre page images

--- src/redux/modules/getGenreActions.js
+++ src/redux/modules/getGenreActions.js
@@ -1,2 +1,17 @@
 import { createAction, handleActions } from 'redux-actions';
+import initialGenreSelectState from '../InitialStates/genrePageState';
 
+// create constants
+export const GET_GENRE_PAGE = 'GET_GENRE_PAGE';
+
+// create actions
+export const getGenrePage = createAction(GET_GENRE_PAGE);
+
+export const actions = {
+  getGenrePage
+};
+
+// create reducers
+export default handleActions({
+  [GET_GENRE_PAGE]: (state, { payload }) => Object.assign({}, state, payload)
+}, initialGenreSelectState);
