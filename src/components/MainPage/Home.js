4a009895e2c01c2304ef7977708c9cde6e7d80ba
Ahmed Modan <ahmed.modan@gmail.com>
2015-12-30 15:18:13 -0800

    update home page for redux refactor

--- src/components/MainPage/Home.js
+++ src/components/MainPage/Home.js
@@ -1,14 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router';
 
-export default () => {
-  return (
-    <div className="row">
-      <div className="container" id="genre-list">
-        <div className="links">
-          <Link to="/genre"> Genre </Link>
+export class MainPage extends React.Component {
+  render() {
+    return (
+      <div className="row">
+        <div className="container" id="genre-list">
+          <div className="links">
+            <Link to="/genre"> Genre </Link>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
